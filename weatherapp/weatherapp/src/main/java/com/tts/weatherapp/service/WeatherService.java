42fa9cac32dab395582377f01b8244fbcabddf42
BrendenBee <Brenden.Brushaber@gmail.com>
2021-08-19 10:58:44 -0500

    Added funcitonality)

--- weatherapp/weatherapp/src/main/java/com/tts/weatherapp/service/WeatherService.java
+++ weatherapp/weatherapp/src/main/java/com/tts/weatherapp/service/WeatherService.java
@@ -1,9 +1,16 @@
 package com.tts.weatherapp.service;
 import com.tts.weatherapp.model.Response;
+import com.tts.weatherapp.model.ZipCode;
+import com.tts.weatherapp.repository.ZipCodeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /*  Our service class will handle all business logic
@@ -17,11 +24,21 @@ public class WeatherService {
     @Value("${api_key}")
     private String apiKey;
 
+    @Autowired
+    ZipCodeRepository zipCodeRepository;
+
+    public WeatherService(ZipCodeRepository zipCodeRepository) {
+        this.zipCodeRepository = zipCodeRepository;
+    }
+
+
+
     public Response getForecast(String zipCode){
         String url = "http://api.openweathermap.org/data/2.5/weather?zip="
                 + zipCode + "&units=imperial&appid=" + apiKey;
         RestTemplate restTemplate = new RestTemplate();
         try {
+            zipCodeRepository.save(new ZipCode(zipCode));
             return restTemplate.getForObject(url, Response.class);
         }
         catch (HttpClientErrorException ex) {
@@ -31,5 +48,18 @@ public class WeatherService {
         }
 
     }
+    /* Method that returns the last 10 zipcode queries */
+
+    public List<ZipCode> getLast10ZipCodes(){
+        List<ZipCode> zipCodeList= (List<ZipCode>) zipCodeRepository.findAll();
+        Collections.reverse(zipCodeList);
+       return zipCodeList.stream()
+                .limit(10)
+                .collect(Collectors.toList());
+
+    }
+
+
+
 
 }
